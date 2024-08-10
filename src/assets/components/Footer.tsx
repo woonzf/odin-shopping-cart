ae2ea013ba21a3b9b9de5c2bdecb19ab893629b0
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-10 19:52:19 +0800

    feat: add Header, Footer and Login components

---
+++ src/assets/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="mx-auto h-12 w-fit p-3">
+        <small>&copy; 2024&nbsp;</small>
+        <a
+          className="underline"
+          href="https://github.com/woonzf"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <small>WZF</small>
+        </a>
+      </div>
+    </footer>
+  );
+}
