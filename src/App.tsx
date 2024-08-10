607399b7bc086c9224993325c666ca45d39cd303
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-10 19:53:06 +0800

    feat: implement basic layout idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import Header from "./assets/components/Header";
+import Footer from "./assets/components/Footer";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [cartAmount, setCartAmount] = useState<number>(0);
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-1">
-      <h1>Our First Test</h1>
-      <button
-        className="rounded-sm border px-2 py-1"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        count is {count}
-      </button>
-      <Link to="profile">Profile page</Link>
-    </div>
+    <>
+      <Header isLogin={isLogin} cartAmount={cartAmount} />
+      <aside></aside>
+      <main className="pt-12"></main>
+      <Footer />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
-import Profile from "./assets/components/Profile.tsx";
-import DefaultProfile from "./assets/components/DefaultProfile.tsx";
-import Spinach from "./assets/components/Spinach.tsx";
+import Login from "./assets/components/Login.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-    ],
-  },
+  { path: "/", element: <App /> },
+  { path: "login", element: <Login /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
