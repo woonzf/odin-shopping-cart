caacf2ce16d73702f5800e24f0ebb6bfb03c18c1
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-12 21:22:47 +0800

    feat: add slide-up and disappear animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,22 @@ export default {
         "light": "#f1f5f9",
       },
       "animation": {
+        "slide-up": "slide-up 2s 1",
         "slide-up-down": "slide-up-down 5s infinite",
+        "disappear": "disappear 750ms 1s 1 forwards",
       },
       "keyframes": {
+        'slide-up': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
         'slide-up-down': {
           '0%, 100%': { transform: 'translateY(100%)' },
           '20%, 90%': { transform: 'translateY(0)' },
+        },
+        'disappear': {
+          '0%': { opacity: '100%' },
+          '100%': { opacity: '0' },
         },
       }
     },
