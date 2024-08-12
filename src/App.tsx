a87089cb1cc40dd2775180249f3122a36b07efc2
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-12 21:27:00 +0800

    feat: update router and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import Home from "./assets/components/Home";
+import { useState } from "react";
+import Intro from "./assets/components/Intro";
+import Header from "../src/assets/components/Header";
+import Footer from "../src/assets/components/Footer";
 import "./App.css";
 
 function App() {
-  return <Home />;
+  const [isIntroEnd, setIsIntroEnd] = useState<boolean>(false);
+  const [cartAmount, setCartAmount] = useState<number>(0);
+
+  return (
+    <>
+      {isIntroEnd === false && (
+        <Intro handleIntroEnd={() => setIsIntroEnd(true)} />
+      )}
+      <Header cartAmount={cartAmount} />
+      <aside></aside>
+      <main className="pt-12"></main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
-import Store from "./assets/components/Store.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "store", element: <Store /> },
+  { path: "cart", element: null },
 ]);
 
 createRoot(document.getElementById("root")).render(
