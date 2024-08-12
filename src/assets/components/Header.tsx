ed430820a2623fe47f4827e4173816e21d8fe45e
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-12 14:24:27 +0800

    feat: add custom colors and animation

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -2,14 +2,14 @@ import { ShoppingBag } from "lucide-react";
 
 export default function Header({ cartAmount }: { cartAmount: number }) {
   return (
-    <header className="fixed z-[99] w-full bg-black bg-opacity-50 text-white">
+    <header className="fixed z-[99] w-full bg-dark text-light">
       <div className="w-xl mx-auto flex h-12 items-center justify-between p-3">
         <div className="text-2xl font-extrabold">MIZU</div>
         <div className="flex gap-5">
           <div>Guest</div>
           <button className="relative">
             <ShoppingBag size={24} />
-            <div className="absolute -bottom-2 -right-2 flex aspect-square h-5 items-center justify-center rounded-full bg-orange-100 pt-[1px] text-black">
+            <div className="absolute -bottom-2 -right-2 flex aspect-square h-5 items-center justify-center rounded-full bg-orange-100 pt-[1px] text-dark">
               {cartAmount}
             </div>
           </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      "colors": {
+        "dark": "#27272a",
+        "light": "#f1f5f9",
+      },
+      "animation": {
+        "slide-up-down": "slide-up-down 5s infinite",
+      },
+      "keyframes": {
+        'slide-up-down': {
+          '0%, 100%': { transform: 'translateY(100%)' },
+          '20%, 90%': { transform: 'translateY(0)' },
+        },
+      }
+    },
   },
   plugins: [],
 }
