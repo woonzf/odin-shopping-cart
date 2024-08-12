cce4be240b626e65311072c3c717326d716dd8d0
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-12 14:39:38 +0800

    feat: update router and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { useState } from "react";
-import Header from "./assets/components/Header";
-import Footer from "./assets/components/Footer";
+import Home from "./assets/components/Home";
 import "./App.css";
 
 function App() {
-  const [cartAmount, setCartAmount] = useState<number>(0);
-
-  return (
-    <>
-      <Header cartAmount={cartAmount} />
-      <aside></aside>
-      <main className="pt-12"></main>
-      <Footer />
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
+import Store from "./assets/components/Store.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
+  { path: "/", element: <App /> },
+  { path: "store", element: <Store /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
