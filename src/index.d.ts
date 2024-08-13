b8c936097a1b642036c24ae3174a33fc7971158d
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-13 20:13:45 +0800

    feat: add types for type checking

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,19 @@
 declare module "*.png";
+
+type category = {
+  id: number;
+  name: string;
+};
+
+type product = {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: {
+    count: number;
+    rate: number;
+  };
+  title: string;
+};
