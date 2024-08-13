3bc1c2991e4e9af6f42f0b36960c8b7220b29da1
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-13 20:12:57 +0800

    refactor: restructure CSS codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,30 @@ export default {
   ],
   theme: {
     extend: {
-      "colors": {
+      fontFamily: {
+        "monserrat": ["Montserrat", "sans-serif"],
+      },
+      colors: {
         "dark": "#27272a",
         "light": "#f1f5f9",
       },
-      "animation": {
+      animation: {
         "slide-up": "slide-up 2s 1",
         "slide-up-down": "slide-up-down 5s infinite",
         "disappear": "disappear 750ms 1s 1 forwards",
       },
-      "keyframes": {
-        'slide-up': {
-          '0%': { transform: 'translateY(100%)' },
-          '100%': { transform: 'translateY(0)' },
+      keyframes: {
+        "slide-up": {
+          "0%": { transform: "translateY(100%)" },
+          "100%": { transform: "translateY(0)" },
         },
-        'slide-up-down': {
-          '0%, 100%': { transform: 'translateY(100%)' },
-          '20%, 90%': { transform: 'translateY(0)' },
+        "slide-up-down": {
+          "0%, 100%": { transform: "translateY(100%)" },
+          "20%, 90%": { transform: "translateY(0)" },
         },
-        'disappear': {
-          '0%': { opacity: '100%' },
-          '100%': { opacity: '0' },
+        "disappear": {
+          "0%": { opacity: "100%" },
+          "100%": { opacity: "0" },
         },
       }
     },
