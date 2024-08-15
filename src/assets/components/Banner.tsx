16de64b571bb479b24e56c5dfbaac3a9172744c5
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-15 18:20:36 +0800

    feat: update Banner component and change its animation

--- src/assets/components/Banner.tsx
+++ src/assets/components/Banner.tsx
@@ -9,19 +9,14 @@ export default function Banner({ categories }: { categories: category[] }) {
   }, 5000);
 
   return (
-    <div className="w-xl mx-auto flex h-[150px] items-center justify-center gap-10 px-6 pt-3">
-      <div className="w-fit">
-        <div className="text-6xl font-extrabold lg:text-7xl">MIZU</div>
-        <div className="flex justify-between px-1.5 text-xl lg:px-2 lg:text-2xl">
-          <span>S</span>
-          <span>T</span>
-          <span>O</span>
-          <span>R</span>
-          <span>E</span>
+    <div className="w-xl mx-auto h-[150px] px-6 pt-3">
+      <div className="mx-auto h-full w-[300px] overflow-hidden lg:w-[500px]">
+        <div className="flex h-full animate-slide-rtl-infinite items-center justify-evenly">
+          <div className="text-center text-lg capitalize">
+            <div>{categories[index].name}</div>
+          </div>
+          <img className="h-full object-cover" src={categories[index].src} />
         </div>
-      </div>
-      <div className="w-[300px] overflow-hidden text-center text-xl capitalize">
-        <div className="animate-slide-up-down">{categories[index].name}</div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ export default {
       },
       animation: {
         "slide-up": "slide-up 2s 1",
-        "slide-up-down": "slide-up-down 5s infinite",
+        "slide-rtl-infinite": "slide-rtl 5s infinite",
         "disappear": "disappear 750ms 1s 1 forwards",
         "appear": "disappear 750ms reverse forwards",
       },
@@ -25,9 +25,10 @@ export default {
           "0%": { transform: "translateY(100%)" },
           "100%": { transform: "translateY(0)" },
         },
-        "slide-up-down": {
-          "0%, 100%": { transform: "translateY(100%)" },
+        "slide-rtl": {
+          "0%": { transform: "translateX(100%)" },
           "20%, 90%": { transform: "translateY(0)" },
+          "100%": { transform: "translateX(-100%)" },
         },
         "disappear": {
           "0%": { opacity: "100%" },
