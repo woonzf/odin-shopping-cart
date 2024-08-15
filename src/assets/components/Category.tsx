847c6080691043d49730ce57e0b3e7cfa329c2ed
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-15 20:29:41 +0800

    feat: update Category component transition in portrait mode

--- src/assets/components/Category.tsx
+++ src/assets/components/Category.tsx
@@ -6,18 +6,33 @@ export default function Category({
 }: {
   handleCategoryChange: any;
 }) {
+  function toggleCategory() {
+    const list: any = document.querySelector("#category-list");
+    const chevron: any = document.querySelector("#category-chevron");
+    if (list) list.classList.toggle("open");
+    if (chevron) chevron.classList.toggle("open");
+  }
+
+  function handleListClick(category: category) {
+    toggleCategory();
+    handleCategoryChange(category);
+  }
+
   return (
     <aside className="sticky top-12 w-[300px] self-start portrait:w-full portrait:bg-theme">
-      <div className="flex items-center justify-between py-3 text-xl font-semibold">
+      <div
+        className="flex items-center justify-between py-3 text-xl font-semibold"
+        onClick={toggleCategory}
+      >
         <div>Category</div>
-        <ChevronDown className="landscape:hidden" />
+        <ChevronDown className="landscape:hidden" id="category-chevron" />
       </div>
-      <ul className="flex flex-col gap-1 portrait:hidden">
+      <ul className="flex flex-col gap-1 overflow-hidden" id="category-list">
         {categories.map((category) => (
           <li key={category.id}>
             <button
               className="w-full bg-light px-3 py-1 text-start capitalize hover:bg-dark hover:text-light"
-              onClick={() => handleCategoryChange(category)}
+              onClick={() => handleListClick(category)}
             >
               {category.name}
             </button>
