dab4e4c2bbc2de2d1e41d6b86742073852e1a915
woonzf <137765412+woonzf@users.noreply.github.com>
2024-08-29 19:58:09 +0800

    feat: update Product layout

--- src/assets/components/Product.tsx
+++ src/assets/components/Product.tsx
@@ -48,9 +48,9 @@ export default function Product({
 
   return (
     <div className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-dark bg-opacity-50 pt-12">
-      <div className="relative flex h-[90%] w-full max-w-screen-lg gap-5 bg-white p-5 portrait:flex-col portrait:gap-2">
+      <div className="relative flex h-[90%] w-full max-w-screen-lg gap-5 overflow-auto bg-white p-5 portrait:flex-col portrait:gap-2">
         <button
-          className="absolute right-5 top-5 z-[99]"
+          className="absolute right-5 top-5 z-[99] landscape:hidden"
           id="close-product"
           onClick={closeProduct}
         >
@@ -58,54 +58,63 @@ export default function Product({
         </button>
         <div className="relative flex w-full items-center justify-center portrait:h-[30%] landscape:p-5">
           <img className="h-[75%] object-contain" src={product.image} />
-          <div className="absolute bottom-0 right-0 flex items-center">
+          <div className="absolute bottom-0 right-0 flex items-center landscape:bottom-3">
             <Star />
             <div className="flex items-center pt-[1px]">
               &nbsp;{product.rating.rate} &#40;{product.rating.count}&#41;
             </div>
           </div>
         </div>
-        <div className="flex w-full flex-col justify-center gap-2 py-10 portrait:py-2">
-          <big className="font-bold">{product.title}</big>
-          <small className="max-h-[100px] overflow-auto">
-            {product.description}
-          </small>
-          <div className="font-bold">$ {product.price.toFixed(2)}</div>
-          <div className="flex flex-col gap-1 landscape:mt-10">
-            <label htmlFor="quantity">Quantity (Max: 10):</label>
-            <div className="flex gap-1">
-              <button
-                className="w-full rounded-md bg-theme hover:bg-orange-200"
-                onClick={handleQuantitySubtract}
-              >
-                -
-              </button>
-              <input
-                className="w-full bg-theme text-center portrait:w-full"
-                type="text"
-                value={quantity}
-                name="quantity"
-                id="quantity"
-                onChange={handleQuantityChange}
-              />
-              <button
-                className="w-full rounded-md bg-theme hover:bg-orange-200"
-                onClick={handleQuantityAdd}
-              >
-                +
-              </button>
+        <div className="flex h-full w-full flex-col justify-between gap-2 py-2">
+          <div className="flex flex-col gap-2">
+            <button
+              className="translate-x-2 self-end pr-5 portrait:hidden"
+              id="close-product"
+              onClick={closeProduct}
+            >
+              <X size={24} />
+            </button>
+            <big className="font-bold">{product.title}</big>
+            <small>{product.description}</small>
+            <div className="font-bold">$ {product.price.toFixed(2)}</div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1 landscape:mt-10">
+              <label htmlFor="quantity">Quantity (Max: 10):</label>
+              <div className="flex gap-1">
+                <button
+                  className="w-full rounded-md bg-theme hover:bg-orange-200"
+                  onClick={handleQuantitySubtract}
+                >
+                  -
+                </button>
+                <input
+                  className="w-full bg-theme text-center portrait:w-full"
+                  type="text"
+                  value={quantity}
+                  name="quantity"
+                  id="quantity"
+                  onChange={handleQuantityChange}
+                />
+                <button
+                  className="w-full rounded-md bg-theme hover:bg-orange-200"
+                  onClick={handleQuantityAdd}
+                >
+                  +
+                </button>
+              </div>
             </div>
+            <div className="flex justify-between text-lg font-bold">
+              <div>Total price:</div>
+              <div>$ {priceTotal.toFixed(2)}</div>
+            </div>
+            <button
+              className="rounded-md bg-green-400 bg-opacity-50 leading-loose hover:bg-green-500"
+              onClick={handleAddToCart}
+            >
+              Add to Cart
+            </button>
           </div>
-          <div className="flex justify-between landscape:mt-10">
-            <div>Total price:</div>
-            <div>$ {priceTotal.toFixed(2)}</div>
-          </div>
-          <button
-            className="rounded-md bg-green-400 bg-opacity-50 leading-loose hover:bg-green-500"
-            onClick={handleAddToCart}
-          >
-            Add to Cart
-          </button>
         </div>
       </div>
     </div>
